2f48f2f74362a0cbe0d9821c1348bf4811be034f
Alexander <acruz@fi.uba.ar>
2025-12-31 13:36:45 -0300

    Se actualizo la carpeta de algoritmos y estructura de datos

--- workspace_2/modulo_1/main.go
+++ workspace_2/modulo_1/main.go
@@ -1,9 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	/*Vamos a hablar sobre las impresiones por pantalla
+	en nuestra consolas tenemos 2 grandes candidatos el primero es
+	el fmt.printf() y despues tenemos el fmt.Println()*/
+	algo := "Algoritmos y Programacion"
+
+	fmt.Println("Esto", "es", algo, 2)
+
+	//Tambien tenemos el fmt.Printf
+	fmt.Printf("Esto es %s %d\n", algo, 2)
 }
